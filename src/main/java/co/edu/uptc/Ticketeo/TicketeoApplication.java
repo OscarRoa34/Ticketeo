641fa26473aec4eebbc6ec281a58004ba156952e
Oscar <oscar.roa03@uptc.edu.co>
2026-02-18 17:27:32 -0500

    first commit

---
+++ src/main/java/co/edu/uptc/Ticketeo/TicketeoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.Ticketeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uptc/Ticketeo/TicketeoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.Ticketeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
