4c05f7f5937a12f3215ead9fd3cf783d9dcb5a78
Davidaguilar03 <david.aguilar89g@gmail.com>
2026-02-24 18:36:31 -0500

    EventCategoryService-Created & EventCategoryModelRepository Fixed

--- src/main/java/co/edu/uptc/Ticketeo/repository/EventCategoryModelRepository.java
+++ src/main/java/co/edu/uptc/Ticketeo/repository/EventCategoryModelRepository.java
@@ -1,9 +1,9 @@
 package co.edu.uptc.Ticketeo.repository;
 
-import co.edu.uptc.Ticketeo.models.EventModel;
+import co.edu.uptc.Ticketeo.models.EventCategoryModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventCategoryModelRepository extends JpaRepository<EventModel, Long> {
+public interface EventCategoryModelRepository extends JpaRepository<EventCategoryModel, Integer> {
 }
