849587be1f32fae6d46b2596ee0963073070f95b
Davidaguilar03 <david.aguilar89g@gmail.com>
2026-03-01 12:49:39 -0500

    feat/36-create-filters-user-page

--- src/main/java/co/edu/uptc/Ticketeo/repository/EventRepository.java
+++ src/main/java/co/edu/uptc/Ticketeo/repository/EventRepository.java
@@ -1,11 +1,18 @@
 package co.edu.uptc.Ticketeo.repository;
 
 import co.edu.uptc.Ticketeo.models.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
+    Page<Event> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    Page<Event> findByCategory_Id(Integer categoryId, Pageable pageable);
+
+    Page<Event> findByNameContainingIgnoreCaseAndCategory_Id(String name, Integer categoryId, Pageable pageable);
 }
 
 
--- src/main/java/co/edu/uptc/Ticketeo/services/EventService.java
+++ src/main/java/co/edu/uptc/Ticketeo/services/EventService.java
@@ -6,8 +6,10 @@ import co.edu.uptc.Ticketeo.repository.EventRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
@@ -31,6 +33,43 @@ public class EventService {
     public Page<Event> getEventsPaginated(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         return eventRepository.findAll(pageable);
+    }
+
+    public Page<Event> getEventsFiltered(String searchQuery, Integer categoryId, int page, int size, String sortType) {
+        Sort sort = Sort.unsorted();
+
+        if (sortType != null) {
+            switch (sortType) {
+                case "price_asc":
+                    sort = Sort.by("price").ascending();
+                    break;
+                case "price_desc":
+                    sort = Sort.by("price").descending();
+                    break;
+                case "date_asc": // Próximos
+                    sort = Sort.by("date").ascending();
+                    break;
+                case "date_desc": // Recientes
+                    sort = Sort.by("date").descending();
+                    break;
+                default:
+                    sort = Sort.by("id").descending();
+            }
+        } else {
+             sort = Sort.by("id").descending();
+        }
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        if (categoryId != null && searchQuery != null && !searchQuery.isEmpty()) {
+            return eventRepository.findByNameContainingIgnoreCaseAndCategory_Id(searchQuery, categoryId, pageable);
+        } else if (categoryId != null) {
+            return eventRepository.findByCategory_Id(categoryId, pageable);
+        } else if (searchQuery != null && !searchQuery.isEmpty()) {
+            return eventRepository.findByNameContainingIgnoreCase(searchQuery, pageable);
+        } else {
+            return eventRepository.findAll(pageable);
+        }
     }
 
     public List<Event> getRandomEvents(int count) {
