ae708cef65188e1ff3163930ec3e5fc92c6fd07d
Oscar <oscar.roa03@uptc.edu.co>
2026-03-01 18:04:45 -0500

    feat/50-create-custom-user-details-service

---
+++ src/main/java/co/edu/uptc/Ticketeo/services/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package co.edu.uptc.Ticketeo.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import co.edu.uptc.Ticketeo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con el nombre: " + username));
+    }
+}
