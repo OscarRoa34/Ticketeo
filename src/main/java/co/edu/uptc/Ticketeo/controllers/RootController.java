3cec8acea7c766901adca457857bf4e04cb6534a
Oscar <oscar.roa03@uptc.edu.co>
2026-03-01 21:54:41 -0500

    feat/52-style-register-login

---
+++ src/main/java/co/edu/uptc/Ticketeo/controllers/RootController.java
@@ -0,0 +1,27 @@
+package co.edu.uptc.Ticketeo.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String rootRedirect(Authentication authentication) {
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return "redirect:/user";
+        }
+
+        for (GrantedAuthority authority : authentication.getAuthorities()) {
+
+            if (authority.getAuthority().equals("ROLE_ADMIN")) {
+                return "redirect:/admin";
+            }
+        }
+
+        return "redirect:/user";
+    }
+}
