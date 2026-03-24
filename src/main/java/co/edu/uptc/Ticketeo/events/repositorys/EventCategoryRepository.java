ff9c3f28f124183236aa775335bf53d789f9d388
Davidaguilar03 <david.aguilar89g@gmail.com>
2026-03-24 16:59:07 -0500

    refactor/71-resources-domain-partitioning

--- src/main/java/co/edu/uptc/Ticketeo/events/repositorys/EventCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uptc.Ticketeo.events.repositorys;
-
-import co.edu.uptc.Ticketeo.events.models.EventCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventCategoryRepository extends JpaRepository<EventCategory, Integer> {
-}
